d65911f1339de945a180e810cafbd9d0470072a7
GMFranceschini <franceschini.gianmarco@gmail.com>
2023-07-17 15:53:57 +0200

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,10 +3,10 @@ return {
   "AstroNvim/astrocommunity",
   {import = "astrocommunity.pack.rust"},
   {import = "astrocommunity.pack.python"},
-  {import = "astrocommunity.colorscheme.kanagawa-nvim"}
+  {import = "astrocommunity.colorscheme.kanagawa-nvim"},
+  {import = "astrocommunity.colorscheme.nord-nvim"},
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -11,5 +11,11 @@ return {
   -- },
   --
   { 'rose-pine/neovim', name = 'rose-pine' },
-  { 'folke/tokyonight.nvim', name = 'tokyonight' }
+  { 'folke/tokyonight.nvim',
+    name = 'tokyonight',
+    opts = {transparent = true} },
+  { 'jpalardy/vim-slime', lazy = false},
+  { 'navarasu/onedark.nvim', lazy = false, style = "darker"},
+  { 'jalvesaq/Nvim-R', lazy = false},
+  { 'xiyaowong/transparent.nvim', lazy = false}
 }
